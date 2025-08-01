69e311adbf30a566c8188a760eac39c671c4147e
Michaelninder <fabianeternis@gmail.com>
2025-08-02 00:37:22 +0200

    hide scrollbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,15 @@
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
         />
+        <style>
+            body::-webkit-scrollbar {
+                display: none;
+            }
+            body {
+                -ms-overflow-style: none;
+                scrollbar-width: 0px;
+            }
+        </style>
     </head>
     <body class="bg-gray-100 text-gray-900">
         @yield('layout-content')
